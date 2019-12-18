4b1d9cc78e28282027fb3c395ea713c17cd0f577
leodini <leodini@hotmail.com>
2019-12-17 22:45:49 -0300

    api requests

--- src/App.vue
+++ src/App.vue
@@ -1,43 +1,49 @@
 <template>
   <div id="app">
+    <Header />
+    <AddTodo v-on:add-todo="addTodo" />
     <Todos v-bind:todos="todos" v-on:del-todo="deleteTodo" />
   </div>
 </template>
 
 <script>
 import Todos from './components/Todos'
-
+import Header from './components/Layout/Header'
+import AddTodo from './components/AddTodo'
+import axios from 'axios'
 
 export default {
   name: 'app',
   components: {
-    Todos
+    Todos,
+    Header,
+    AddTodo
   },
   data(){
     return {
-      todos: [
-        {
-          id: 1,
-          title: 'Todo one',
-          completed: false
-        },
-        {
-          id: 2,
-          title: 'todo two',
-          completed: false
-        },
-        {
-          id: 3,
-          title: 'todo three',
-          completed: false
-        }
-      ]
+      todos: []
     }
   },
   methods: {
     deleteTodo(id){
-      this.todos = this.todos.filter(todo => todo.id !== id);
+      axios.delete(`https://jsonplaceholder.typicode.com/todos/${id}`)
+      .then(res => this.todos = this.todos.filter(todo => todo.id !== id))
+      .catch(err => console.log(err))
+      // 
+    },
+    addTodo(newTodo){
+      const { title, completed } = newTodo;
+      axios.post('https://jsonplaceholder.typicode.com/todos', {
+        title, completed
+      })
+      .then(res => this.todos = [...this.todos, res.data])
+      .catch(err => console.log(err))
     }
+  },
+  created() {
+    axios.get('https://jsonplaceholder.typicode.com/todos?_limit=10')
+    .then(res => this.todos = res.data)
+    .catch(err => console.log(err))
   }
 }
 </script>
@@ -52,5 +58,18 @@ export default {
   body{
     font-family: Arial, Helvetica, sans-serif;
     line-height: 1.4;
+  }
+
+  .btn{
+    display: inline-block;
+    border: none;
+    background: #555;
+    color: #fff;
+    padding: 7px 20px;
+    cursor: pointer
+  }
+
+  .btn:hover{
+    background: #666;
   }
 </style>
